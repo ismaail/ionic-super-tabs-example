947401c152603f79686f3950218399e653b22913
ismaail <contact@ismaail.com>
2021-03-19 21:47:52 +0100

    Add Button 'GOTO TAB 2' in PROFILE TAB

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SuperTabs } from '@ionic-super-tabs/angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(public superTabs: SuperTabs) { }
 
   ngOnInit() {
   }
